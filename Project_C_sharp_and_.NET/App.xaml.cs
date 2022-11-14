a27fb4554d2c2bb8f7fac65a94ec70e05c379348
Jens Kersemans <jens.kersemans@telenet.be>
2022-11-14 12:29:38 +0100

    Add project files.

---
+++ Project_C_sharp_and_.NET/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_C_sharp_and_.NET;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Project_C_sharp_and_.NET/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Project_C_sharp_and_.NET;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
