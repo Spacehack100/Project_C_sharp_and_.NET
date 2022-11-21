54e42e69bbaec98a94ff8e6be896bb8fb76b5ed9
Jens Kersemans <jens.kersemans@telenet.be>
2022-11-21 10:34:15 +0100

    Added base viewmodel, api and test datastore classes (methods not written)

--- Project_C_sharp_and_.NET/App.xaml.cs
+++ Project_C_sharp_and_.NET/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace Project_C_sharp_and_.NET;
+﻿using CoreMotion;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Project_C_sharp_and_.NET;
 
 public partial class App : Application
 {
@@ -6,6 +10,99 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
+		DependencyService.Register<TestDataStore>();
 		MainPage = new AppShell();
 	}
+}
+
+public interface IDataStore
+{
+	public List<Item> itemList { get; set; }
+
+	String AddItem(Item itemToAdd);
+	Item GetItembyName(string name);
+	String EditItem(string name);
+	String DeleteItem(string name);
+}
+
+public class BaseViewModel : INotifyPropertyChanged
+{
+	public IDataStore DataStore => DependencyService.Get<IDataStore>();
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+	protected void OnPropetyChanged([CallerMemberName] string name=null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+	}
+}
+
+class TestDataStore : IDataStore
+{
+	public List<Item> itemList { get; set; }
+    
+	public TestDataStore()
+	{
+		itemList = new List<Item>();
+	}
+
+    public String AddItem(Item itemToAdd)
+	{
+		return "not done";
+	}
+
+    public Item GetItembyName(string name)
+	{
+        Item temp = new Item();
+        return temp;
+    }
+
+    public String EditItem(string name)
+	{
+        return "not done";
+    }
+
+    public String DeleteItem(string name)
+	{
+        return "not done";
+    }
+}
+
+class ApiDataStore : IDataStore
+{
+    public List<Item> itemList { get; set; }
+
+    public ApiDataStore()
+    {
+        itemList = new List<Item>();
+    }
+
+    public String AddItem(Item itemToAdd)
+    {
+        return "not done";
+    }
+
+    public Item GetItembyName(string name)
+    {
+		Item temp = new Item();
+        return temp;
+    }
+
+    public String EditItem(string name)
+    {
+        return "not done";
+    }
+
+    public String DeleteItem(string name)
+    {
+        return "not done";
+    }
+}
+
+public struct Item
+{
+	string name { get; set; }
+	int value1 { get; set; }
+	int value2 { get; set; }
+	string value3 { get; set; }
 }
