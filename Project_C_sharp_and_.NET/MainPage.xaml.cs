1fbf0e776bbfc25f4665bc8d5ee5939f152f3cb8
Jens Kersemans <jens.kersemans@telenet.be>
2022-12-06 15:04:59 +0100

    Formated list to show everything and be clearer

--- Project_C_sharp_and_.NET/MainPage.xaml.cs
+++ Project_C_sharp_and_.NET/MainPage.xaml.cs
@@ -5,7 +5,6 @@ namespace Project_C_sharp_and_.NET;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 	MainViewModel mvm;
 
 	public MainPage()
@@ -13,18 +12,6 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 		BindingContext = mvm = new MainViewModel();
 
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 }
 
